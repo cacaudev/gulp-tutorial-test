084506f44e9a586992411c36fa4145a11d763dee
cacaudev <cacauabreu2013@gmail.com>
2019-09-15 01:12:12 -0300

    Changed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,6 @@
 var gulp = require('gulp'),
     browsersync = require("browser-sync").create();
 
-var paths = {
-  src: './src/**/*',
-  srcHTML: './src/**/*.html',
-  srcCSS: './src/**/*.css',
-  srcJS: './src/**/*.js',  
-
-  tmp: './tmp/', // tmp folder
-  tmpIndex: './tmp/index.html', // index.html in tmp folder
-  tmpCSS: './tmp/**/*.css', // css files in tmp folder
-  tmpJS: './tmp/**/*.js', // js files in tmp folder  dist: 'dist',
-  
-  distIndex: 'dist/index.html',
-  distCSS: 'dist/**/*.css',
-  distJS: 'dist/**/*.js'
-};
-
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
@@ -37,22 +21,22 @@ function browserSync(done) {
 // Copy all html files to tmp
 function html() {
   return gulp
-    .src(paths.srcHTML)
-    .pipe(gulp.dest(paths.tmp));
+    .src('./src/**/*.html')
+    .pipe(gulp.dest('./tmp/'));
 }
 
 // Copy all css files to tmp
 function css() {
   return gulp
-  .src(paths.srcCSS)
-  .pipe(gulp.dest(paths.tmp));
+  .src('./src/**/*.css')
+  .pipe(gulp.dest('./tmp/'));
 }
 
 // Copy all js files to tmp
 function js() {
   return gulp
-    .src(paths.srcJS)
-    .pipe(gulp.dest(paths.tmp));
+    .src('./src/**/*.js')
+    .pipe(gulp.dest('./tmp/'));
 }
 
 // Watch files and rerun the copy tasks to tmp, and reload the page
